9b92a477797969f6f257a8d7f887daca4a15ce75
xlimitan <xlimitan@yandex.ru>
2023-03-18 17:18:05 +0300

    hi lvl

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,37 @@
-
 public class Main {
-
-    public static Employee employees[] = new Employee[10];
-
     public static void main(String[] args) {
+        EmployeeBook employeeBook = new EmployeeBook();
 
-
-        Employee employee1 = new Employee("Ivan", "Ivanov", "Ivanovichd", 1, 40000);
-        Employee employee2 = new Employee("Roman", "Romanov", "Romanovich", 2, 36000);
-        Employee employee3 = new Employee("Oleg", "Olegov", "Olegovich", 3, 37000);
-
-
-        employees[0] = employee1;
-        employees[1] = employee2;
-        employees[2] = employee3;
-
-        System.out.println(allEmployee(employees));
-        System.out.println("Общая стоимость зарплаты " + allSalary(employees) + " рублей");
-        System.out.println(minSalary(employees));
-        System.out.println(maxSalary(employees));
-        System.out.println("Средня стоимость зарплаты " + allSalary(employees) / allEmployeers(employees)  + " рублей");
-        System.out.println(allEmpl(employees));
-
-
-    }
-    public static String allEmployee(Employee[] employee) {
-        for (Employee i : employee
-        ) {
-            if (i != null) {
-                System.out.println(i.toString());
-            }
-        }
-        return "";
-    }
-    public static String allEmpl(Employee[] employee) {
-        for (Employee e : employee
-        ) {
-            if (e != null) {
-                System.out.println(e.toString1());
-            }
-        }
-        return "";
-    }
-    public static int allSalary(Employee[] sal) {
-        int sum = 0;
-        for (Employee i : sal) {
-            if (i != null) {
-                sum+=i.getSalary();
-            }
-        }
-        return sum;
-    }
-    public static String minSalary(Employee[] employee) {
-        int min=0;
-        int idEmployee=0;
-        for (int i = 0; i < employees.length; i++) {
-            if(employees[i] != null) {
-                min = (int) employees[i].getSalary();
-            }
-        }
-        for (int i = 0; i < employees.length; i++) {
-            if(employees[i] != null) {
-                if (employees[i].getSalary() < min){
-                    min = (int) employees[i].getSalary();
-                    idEmployee = i;
-                }
-            }
-        }
-        return "Сотрудник с минимальной зарплатой " + employee[idEmployee];
-    }
-    public static String maxSalary(Employee[] employee) {
-        int max=0;
-        int idEmployee=0;
-        for (int i = 0; i < employees.length; i++) {
-            if(employees[i] != null) {
-                if (employees[i].getSalary() > max){
-                    max = (int) employees[i].getSalary();
-                    idEmployee = i;
-                }
-            }
-        }
-        return "Сотрудник с максималной зарплатой " + employee[idEmployee];
-    }
-    public static int allEmployeers(Employee[] employee) {
-        int num = 0;
-        for (Employee i : employee) {
-            if (i != null) {
-                num++;
-            }
-        }
-        return num;
+        employeeBook.printAll();
+        System.out.println();
+        employeeBook.remove(3);
+        employeeBook.printAll();
+        employeeBook.add(new Employee("Vano Vanov Vanovich",2, 31000));
+        System.out.println();
+        employeeBook.printAll();
+        System.out.println("Общие затраты на запралаты в месяц " +  employeeBook.totalSalary() + " рублей");
+        System.out.println(employeeBook.minSalary());
+        System.out.println(employeeBook.maxSalary());
+        System.out.println(employeeBook.averageSalary());
+        employeeBook.printAllNames();
+        employeeBook.increaseByAmount(0.1);
+        employeeBook.printAll();
+        System.out.println();
+        System.out.println(employeeBook.minSalary(1));
+        System.out.println();
+        System.out.println(employeeBook.maxSalary(2));
+        System.out.println();
+        System.out.println(employeeBook.totalSalary(2));
+        System.out.println();
+        System.out.println(employeeBook.averageSalary(2));
+        System.out.println();
+        employeeBook.increaseByAmount(0.1,1);
+        employeeBook.printAll();
+        System.out.println();
+        employeeBook.printAll(4);
+        System.out.println();
+        employeeBook.printEmployeeMinSalary(30000);
+        System.out.println();
+        employeeBook.printEmployeeMaxSalary(50000);
     }
 }
