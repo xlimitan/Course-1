b0ea2c27015ecf98fc51a0be7decc0dca9291cd5
xlimitan <xlimitan@yandex.ru>
2023-03-18 17:18:05 +0300

    hi lvl

--- src/Employee.java
+++ src/Employee.java
@@ -1,53 +1,25 @@
 public class Employee {
-    private String firsName;
-    private String midName;
-    private String lastName;
+    private String fullName;
     private int department;
-    private int salary;
-    private int id;
+    private double salary;
+    private final long id;
     static int count = 1;
 
-    public Employee(String firsName, String midName, String lastName, int department, int salary) {
-        this.firsName = firsName;
-        this.midName = midName;
-        this.lastName = lastName;
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
         this.department = department;
         this.salary = salary;
-        id = count;
-            count++;
+        this.id = count++;
     }
-
-    public String getFirsName() {
-        return firsName;
-    }
-    public String getMidName() {
-        return midName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public int getDepartment() {
-        return department;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setDepartment(int department) {
-        this.department = department;
-    }
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+    public String getFullName() { return fullName; }
+    public int getDepartment() { return department; }
+    public double getSalary() { return salary; }
+    public long getId() { return id; }
+    public void setDepartment(int department) { this.department = department; }
+    public void setSalary(double salary) { this.salary = salary; }
 
     @Override
     public String toString() {
-        return "Сотрудник " + firsName + " " + midName + " " + lastName + ", отдел № " + department + ", зарплата " + salary + " рублей, id=" + id;
-    }
-    public String toString1() {
-        return "Сотрудник " + firsName + " " + midName + " " + lastName;
+        return "ФИО сотрудника " + fullName + ", Отдел № " + department + ", Зарплата " + salary + " рублей, id " + id;
     }
 }
