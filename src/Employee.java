f0774d12b563cf826efba42a5dbd997e03a38913
Semen Golenko <123659683+xlimitan@users.noreply.github.com>
2023-03-18 17:52:58 +0300

    Revert "hi lvl"
    This reverts commit 9b92a477797969f6f257a8d7f887daca4a15ce75.

--- src/Employee.java
+++ src/Employee.java
@@ -1,25 +1,53 @@
 public class Employee {
-    private String fullName;
+    private String firsName;
+    private String midName;
+    private String lastName;
     private int department;
-    private double salary;
-    private final long id;
+    private int salary;
+    private int id;
     static int count = 1;
 
-    public Employee(String fullName, int department, double salary) {
-        this.fullName = fullName;
+    public Employee(String firsName, String midName, String lastName, int department, int salary) {
+        this.firsName = firsName;
+        this.midName = midName;
+        this.lastName = lastName;
         this.department = department;
         this.salary = salary;
-        this.id = count++;
+        id = count;
+            count++;
     }
-    public String getFullName() { return fullName; }
-    public int getDepartment() { return department; }
-    public double getSalary() { return salary; }
-    public long getId() { return id; }
-    public void setDepartment(int department) { this.department = department; }
-    public void setSalary(double salary) { this.salary = salary; }
+
+    public String getFirsName() {
+        return firsName;
+    }
+    public String getMidName() {
+        return midName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
     @Override
     public String toString() {
-        return "ФИО сотрудника " + fullName + ", Отдел № " + department + ", Зарплата " + salary + " рублей, id " + id;
+        return "Сотрудник " + firsName + " " + midName + " " + lastName + ", отдел № " + department + ", зарплата " + salary + " рублей, id=" + id;
+    }
+    public String toString1() {
+        return "Сотрудник " + firsName + " " + midName + " " + lastName;
     }
 }
